a02cd5655d8084169fe3439f31d6d0481b989e48
Augustin Floren <augustinfloren@gmail.com>
2024-03-07 16:55:55 +0100

    Ajout de flêches et défilement sur le carrousel

--- wp-content/plugins/pdb-carrousel/js/pdb-carrousel-video.js
+++ wp-content/plugins/pdb-carrousel/js/pdb-carrousel-video.js
@@ -16,29 +16,62 @@ class Carrousel {
             slidesVisible: 1
         }, options);
         let children = [].slice.call(element.children);
-        let root = this.createDivWithClass("pdb-carrousel");
+        this.currentItem = 0;
+        this.root = this.createDivWithClass("pdb-carrousel");
         this.container = this.createDivWithClass("pdb-carrousel-container");
-        root.appendChild(this.container);
-        this.element.appendChild(root);
+        this.root.appendChild(this.container);
+        this.element.appendChild(this.root);
         this.items = children.map((child) => {
             let item = this.createDivWithClass('pdb-carrousel-item');
             item.appendChild(child);
             this.container.appendChild(item);
             return item;
         })
-        this.setStyle()
+        this.setStyle();
+        this.createNavigation();
     }
 
+    /**
+     * Applique les bonnnes dimensions aux elem du carrousel
+     */
     setStyle() {
         let ratio = this.items.length / this.options.slidesVisible;
         this.container.style.width = (ratio * 100) + "%";
         this.items.forEach(item => item.style.width = ((100 / this.options.slidesVisible) / ratio) + "%");
+    };
+
+    createNavigation() {
+        let nextButton = this.createDivWithClass("pdb-carrousel-next");
+        let prevButton = this.createDivWithClass("pdb-carrousel-prev");
+        this.root.appendChild(nextButton);
+        this.root.appendChild(prevButton);
+        nextButton.addEventListener('click', this.next.bind(this));
+        prevButton.addEventListener('click', this.prev.bind(this))
+    };
+
+    next() {
+        this.goToItem(this.currentItem + this.options.slidesToScroll);
+    }
+
+    prev() {
+        this.goToItem(this.currentItem - this.options.slidesToScroll);
     }
 
     /**
-     * 
+     * déplace le carrousel vers l'élément ciblé
+     * @param {number} index 
+     */
+
+    goToItem(index) {
+        let translateX = index * 100 / this.items.length;
+        this.container.style.transform = 'translate3d(' + translateX + '%, 0, 0)';
+        this.currentItem = index;
+    }
+
+    /**
      * @param {string} className 
      * @returns {HTMLelement}
+     * 
      */
 
     createDivWithClass(className) {
