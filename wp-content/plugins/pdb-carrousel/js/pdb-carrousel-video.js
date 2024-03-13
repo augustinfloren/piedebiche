80e32c68d5383c298cbf345d5809cd9be582cc58
Augustin Floren <augustinfloren@gmail.com>
2024-03-13 08:58:27 +0100

    défilement infini carrousel OK, ajout d'un masque transparent

--- wp-content/plugins/pdb-carrousel/js/pdb-carrousel-video.js
+++ wp-content/plugins/pdb-carrousel/js/pdb-carrousel-video.js
@@ -1,103 +1,262 @@
-class Carrousel {
-
+class Carousel {
     /**
-     * 
-     * @param {HTMLElement} element 
-     * @param {Object} options 
-     * @param {Object} options.slidesToScroll Nombre d'éléments à faire défiler
-     * @param {Object} options.slidesToScroll Nombre d'éléments visibles dans le slide
-     * 
+     * This callback type is called `requestCallback` and is displayed as a global symbol.
+     *
+     * @callback moveCallback
+     * @param {number} index
      */
-
+  
+    /**
+     * @param {HTMLElement} element
+     * @param {Object} options
+     * @param {Object} [options.slidesToScroll=1] Nombre d'éléments à faire défiler
+     * @param {Object} [options.slidesVisible=1] Nombre d'éléments visible dans un slide
+     * @param {boolean} [options.loop=false] Doit-t-on boucler en fin de carousel
+     * @param {boolean} [options.infinite=false]
+     * @param {boolean} [options.pagination=false]
+     * @param {boolean} [options.navigation=true]
+     */
     constructor (element, options = {}) {
-        this.element = element
-        this.options = Object.assign({}, {
-            slidesToScroll: 1,
-            slidesVisible: 1
-        }, options);
-        let children = [].slice.call(element.children);
-        this.isMobile = false;
-        this.currentItem = 0;
-
-        // Modification du DOM
-        this.root = this.createDivWithClass("pdb-carrousel");
-        this.container = this.createDivWithClass("pdb-carrousel-container");
-        this.root.setAttribute('tabindex', '1');
-        this.root.appendChild(this.container);
-        this.element.appendChild(this.root);
-        this.items = children.map((child) => {
-            let item = this.createDivWithClass('pdb-carrousel-item');
-            item.appendChild(child);
-            this.container.appendChild(item);
-            return item;
-        })
-        this.setStyle();
-        this.createNavigation();
-
-        // événements 
-        this.root.addEventListener('keyup', (e) => {
-            if (e.key === 'ArrowRight') {
-                this.next();
-            } else if (e.key === 'ArrowLeft') {
-                this.prev();
-            }
-        });
+      this.element = element
+      this.options = Object.assign({}, {
+        slidesToScroll: 1,
+        slidesVisible: 1,
+        loop: false,
+        pagination: false,
+        navigation: true,
+        infinite: false
+      }, options)
+      if (this.options.loop && this.options.infinite) {
+        throw new Error('Un carousel ne peut être à la fois en boucle et en infinie')
+      }
+      let children = [].slice.call(element.children)
+      this.isMobile = false
+      this.currentItem = 0
+      this.moveCallbacks = []
+      this.offset = 0
+  
+      // Modification du DOM
+      this.root = this.createDivWithClass('pdb-carrousel')
+      this.container = this.createDivWithClass('pdb-carrousel-container')
+      this.root.setAttribute('tabindex', '0')
+      this.root.appendChild(this.container)
+      this.element.appendChild(this.root)
+      this.items = children.map((child) => {
+        let item = this.createDivWithClass('pdb-carrousel-item')
+        item.appendChild(child)
+        return item
+      })
+      if (this.options.infinite) {
+        this.offset = this.options.slidesVisible + this.options.slidesToScroll
+        if (this.offset > children.length) {
+          console.error("Vous n'avez pas assez d'élément dans le carousel", element)
+        }
+        this.items = [
+          ...this.items.slice(this.items.length - this.offset).map(item => item.cloneNode(true)),
+          ...this.items,
+          ...this.items.slice(0, this.offset).map(item => item.cloneNode(true)),
+        ]
+        this.gotoItem(this.offset, false)
+      }
+      this.items.forEach(item => this.container.appendChild(item))
+      this.setStyle()
+      if (this.options.navigation) {
+        this.createNavigation()
+      }
+      if (this.options.pagination) {
+        this.createPagination()
+      }
+  
+      // Evenements
+      this.moveCallbacks.forEach(cb => cb(this.currentItem))
+      this.onWindowResize()
+      window.addEventListener('resize', this.onWindowResize.bind(this))
+      this.root.addEventListener('keyup', e => {
+        if (e.key === 'ArrowRight' || e.key === 'Right') {
+          this.next()
+        } else if (e.key === 'ArrowLeft' || e.key === 'Left') {
+          this.prev()
+        }
+      })
+      if (this.options.infinite) {
+        this.container.addEventListener('transitionend', this.resetInfinite.bind(this))
+      }
     }
-
+  
     /**
-     * Applique les bonnnes dimensions aux elem du carrousel
+     * Applique les bonnes dimensions aux éléments du carousel
      */
-    setStyle() {
-        let ratio = this.items.length / this.options.slidesVisible;
-        this.container.style.width = (ratio * 100) + "%";
-        this.items.forEach(item => item.style.width = ((100 / this.options.slidesVisible) / ratio) + "%");
-    };
-
-    createNavigation() {
-        let navContainer = this.createDivWithClass("pdb-carrousel-nav-container");
-        let nextButton = this.createDivWithClass("pdb-carrousel-next");
-        let prevButton = this.createDivWithClass("pdb-carrousel-prev");
-        navContainer.appendChild(prevButton);
-        navContainer.appendChild(nextButton);
-        this.root.appendChild(navContainer);
-        nextButton.addEventListener('click', this.next.bind(this));
-        prevButton.addEventListener('click', this.prev.bind(this))
-    };
-
-    next() {
-        this.goToItem(this.currentItem - this.options.slidesToScroll);
+    setStyle () {
+      let ratio = this.items.length / this.slidesVisible
+      this.container.style.width = (ratio * 100) + "%"
+      this.items.forEach(item => item.style.width = ((100 / this.slidesVisible) / ratio) + "%")
     }
-
-    prev() {
-        this.goToItem(this.currentItem + this.options.slidesToScroll);
-    }
-
+  
     /**
-     * déplace le carrousel vers l'élément ciblé
-     * @param {number} index 
+     * Crée les flêches de navigation dans le DOM
      */
-
-    goToItem(index) {
-        let translateX = index * 100 / this.items.length;
-        this.container.style.transform = 'translate3d(' + translateX + '%, 0, 0)';
-        this.currentItem = index;
+    createNavigation () {
+      let navContainer = this.createDivWithClass("pdb-carrousel-nav-container");
+      let nextButton = this.createDivWithClass('pdb-carrousel-next')
+      let prevButton = this.createDivWithClass('pdb-carrousel-prev')
+      navContainer.appendChild(prevButton);
+      navContainer.appendChild(nextButton);
+      this.root.appendChild(navContainer);
+      nextButton.addEventListener('click', this.next.bind(this))
+      prevButton.addEventListener('click', this.prev.bind(this))
+      if (this.options.loop === true) {
+        return
+      }
+      this.onMove(index => {
+        if (index === 0) {
+          prevButton.classList.add('pdb-carrousel-prev-hidden')
+        } else {
+          prevButton.classList.remove('pdb-carrousel-prev-hidden')
+        }
+        if (this.items[this.currentItem + this.slidesVisible] === undefined) {
+          nextButton.classList.add('pdb-carrousel-next-hidden')
+        } else {
+          nextButton.classList.remove('pdb-carrousel-next-hidden')
+        }
+      })
     }
-
+  
     /**
-     * @param {string} className 
-     * @returns {HTMLelement}
-     * 
+     * Crée la pagination dans le DOM
      */
-
-    createDivWithClass(className) {
-        let div = document.createElement('div');
-        div.setAttribute('class', className);
-        return div;
+    createPagination () {
+      let pagination = this.createDivWithClass('pdb-carrousel-pagination')
+      let buttons = []
+      this.root.appendChild(pagination)
+      for (let i = 0; i < (this.items.length - 2 * this.offset); i = i + this.options.slidesToScroll) {
+        let button = this.createDivWithClass('pdb-carrousel-pagination-button')
+        button.addEventListener('click', () => this.gotoItem(i + this.offset))
+        pagination.appendChild(button)
+        buttons.push(button)
+      }
+      this.onMove(index => {
+        let count = this.items.length - 2 * this.offset
+        let activeButton = buttons[Math.floor(((index - this.offset) % count) / this.options.slidesToScroll)]
+        if (activeButton) {
+          buttons.forEach(button => button.classList.remove('pdb-carrousel-pagination-button-active'))
+          activeButton.classList.add('pdb-carrousel-pagination-button-active')
+        }
+      })
     }
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-
-    new Carrousel(document.getElementById("pdb-carrousel-video"));
-
-});
+  
+    /**
+     *
+     */
+    next () {
+      this.gotoItem(this.currentItem + this.slidesToScroll)
+    }
+  
+    prev () {
+      this.gotoItem(this.currentItem - this.slidesToScroll)
+    }
+  
+    /**
+     * Déplace le carousel vers l'élément ciblé
+     * @param {number} index
+     * @param {boolean} [animation = true]
+     */
+    gotoItem (index, animation = true) {
+      if (index < 0) {
+        if (this.options.loop) {
+          index = this.items.length - this.slidesVisible
+        } else {
+          return
+        }
+      } else if (index >= this.items.length || (this.items[this.currentItem + this.slidesVisible] === undefined && index > this.currentItem)) {
+        if (this.options.loop) {
+          index = 0
+        } else {
+          return
+        }
+      }
+      let translateX = index * -100 / this.items.length
+      if (animation === false) {
+        this.container.style.transition = 'none'
+      }
+      this.container.style.transform = 'translate3d(' + translateX + '%, 0, 0)'
+      this.container.offsetHeight // force repaint
+      if (animation === false) {
+        this.container.style.transition = ''
+      }
+      this.currentItem = index
+      this.moveCallbacks.forEach(cb => cb(index))
+    }
+  
+    /**
+     * Déplace le container pour donner l'impression d'un slide infini
+     */
+    resetInfinite () {
+      if (this.currentItem <= this.options.slidesToScroll) {
+        this.gotoItem(this.currentItem + (this.items.length - 2 * this.offset), false)
+      } else if (this.currentItem >= this.items.length - this.offset) {
+        this.gotoItem(this.currentItem - (this.items.length - 2 * this.offset), false)
+      }
+    }
+  
+    /**
+     * Rajoute un écouteur qui écoute le déplacement du carousel
+     * @param {moveCallback} cb
+     */
+    onMove (cb) {
+      this.moveCallbacks.push(cb)
+    }
+  
+    /**
+     * Ecouteur pour le redimensionnement de la fenêtre
+     */
+    onWindowResize () {
+      let mobile = window.innerWidth < 800
+      if (mobile !== this.isMobile) {
+        this.isMobile = mobile
+        this.setStyle()
+        this.moveCallbacks.forEach(cb => cb(this.currentItem))
+      }
+    }
+  
+    /**
+     * Helper pour créer une div avec une classe
+     * @param {string} className
+     * @returns {HTMLElement}
+     */
+    createDivWithClass (className) {
+      let div = document.createElement('div')
+      div.setAttribute('class', className)
+      return div
+    }
+  
+    /**
+     * @returns {number}
+     */
+    get slidesToScroll () {
+      return this.isMobile ? 1 : this.options.slidesToScroll
+    }
+  
+    /**
+     * @returns {number}
+     */
+    get slidesVisible () {
+      return this.isMobile ? 1 : this.options.slidesVisible
+    }
+  
+  }
+  
+  let onReady = function () {
+  
+    new Carousel(document.querySelector('#pdb-carrousel-video'), {
+        slidesVisible: 1,
+        slidesToScroll: 1,
+        infinite: true,
+    })
+  
+  }
+  
+  if (document.readyState !== 'loading') {
+    onReady()
+  }
+  document.addEventListener('DOMContentLoaded', onReady)
+  
