fcc08a266eca81e8a69a541117c27252f58f05ed
Augustin Floren <augustinfloren@gmail.com>
2024-07-30 15:24:41 +0200

    Ajout fullscreen sur carrousel photo

--- wp-content/plugins/pdb-carrousel/js/pdb-photos.js
+++ wp-content/plugins/pdb-carrousel/js/pdb-photos.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
 const section = document.getElementById("pdb-photos");
     const slider = document.createElement("div");
     slider.classList.add("swiper");
+    slider.classList.add("normal");
     slider.setAttribute("id", "carrousel-photo");
     const wrapper = document.createElement("div");
     wrapper.classList.add("swiper-wrapper");
@@ -27,7 +28,9 @@ const section = document.getElementById("pdb-photos");
             });
             section.appendChild(slider);
             function initSwiper() {
-                const swiper = new Swiper("#carrousel-photo", {
+                let fsSwiper;
+                let swiper;
+                swiper = new Swiper("#carrousel-photo", {
                     centeredSlides: true,
                     loop: true,
                     spaceBetween: 50,
@@ -41,30 +44,124 @@ const section = document.getElementById("pdb-photos");
                     }
                 });
 
-                function handleSlideChange() {
-                    if (swiper.slides[swiper.previousIndex]) {
-                        const previousSlide = swiper.slides[swiper.previousIndex];
-                        const photo = previousSlide.querySelector(".photo");
-                        photo.style.cursor = "initial";
+                let fs = false;
+
+                function handleFirstSlide(slider) {
+                    const activeSlide = slider.slides[slider.activeIndex];
+                    const activePhoto = activeSlide.querySelector(".photo");
+                    activePhoto.style.cursor = "pointer";
+                    activePhoto.addEventListener("click", enterFsMode);
+                }
+
+                function handleSlideChange(slider) {
+                    if (slider.slides[slider.previousIndex]) {
+                        const previousSlide = slider.slides[slider.previousIndex];
+                        const previousPhoto = previousSlide.querySelector(".photo");
+                        previousPhoto.style.cursor = "initial";
+                        if (!fs) {
+                            previousPhoto.removeEventListener("click", enterFsMode);
+                        } else {
+                            previousPhoto.removeEventListener("click", quitFsMode);
+                        }
                     }
 
-                    if (swiper.slides[swiper.activeIndex]) {
-                        const activeSlide = swiper.slides[swiper.activeIndex];
-                        const photo = activeSlide.querySelector(".photo");
-                        photo.style.cursor = "pointer";
+                    if (slider.slides[slider.activeIndex]) {
+                        const activeSlide = slider.slides[slider.activeIndex];
+                        const activePhoto = activeSlide.querySelector(".photo");
+                        activePhoto.style.cursor = "pointer";
+                        if (!fs) {
+                            activePhoto.addEventListener("click", enterFsMode);
+                        } else {
+                            activePhoto.addEventListener("click", quitFsMode);
+                        }
                     }
                 }
 
-                function blowPhoto(event) {
-                    const activeSlide = swiper.slides[swiper.activeIndex];
-                    const photo = activeSlide.querySelector(".photo");
+                function enterFsMode(event) {
+                    const currentIndex = swiper.realIndex;
+                    swiper.destroy(true, true);
+                    fsSwiper = new Swiper("#carrousel-photo", {
+                        centeredSlides: true,
+                        loop: true,
+                        spaceBetween: 50,
+                        slidesPerView: 1,
+                        initialSlide: currentIndex,
+                        speed: 400,
+                        navigation: false,
+                    });
+                    nextBtn.addEventListener("click", () => {
+                        fsSwiper.slideNext();
+                    });
+                    prevBtn.addEventListener("click", () => {
+                        fsSwiper.slidePrev();
+                    });
+
+                    handleFirstSlide(fsSwiper);
+                    fsSwiper.on('slideChangeTransitionStart', () => {
+                        handleSlideChange(fsSwiper)
+                    });
+                    fs = true;
+                    event.currentTarget.removeEventListener("click", enterFsMode);
+                    event.currentTarget.addEventListener("click", quitFsMode);
+                    const photos = document.querySelectorAll(".photo");
+                    photos.forEach((photo) => {
+                        photo.style.height = "100%";
+                        photo.style.width = "100%";
+                    });
+                    const main = document.querySelector("main");
+                    main.style.overflowY = "hidden";
+                    const logo = document.getElementById("pdb-logo-container");
+                    const burger = document.getElementById("pdb-burger");
+                    burger.style.zIndex = "unset";
+                    logo.style.zIndex = "unset";
+                    slider.classList.remove("normal");
+                    slider.classList.add("full-screen");
                 }
 
-                function handleFirstSlide() {
-                    const activeSlide = swiper.slides[swiper.activeIndex];
-                    const photo = activeSlide.querySelector(".photo");
-                    photo.style.cursor = "pointer";
-                    photo.addEventListener("click", blowPhoto);
+                function quitFsMode(event) {
+                    // swiper.params.breakpoints[800].slidesPerView = 2.2;
+                    const currentIndex = fsSwiper.realIndex;
+                    fsSwiper.destroy(true, true);
+                    swiper = new Swiper("#carrousel-photo", {
+                        centeredSlides: true,
+                        loop: true,
+                        spaceBetween: 50,
+                        slidesPerView: 1,
+                        initialSlide: currentIndex,
+                        speed: 400,
+                        navigation: false,
+                        breakpoints: {
+                            800: {
+                            slidesPerView: 2.2,
+                            }
+                        }
+                    });
+                    nextBtn.addEventListener("click", () => {
+                        swiper.slideNext();
+                    });
+                    prevBtn.addEventListener("click", () => {
+                        swiper.slidePrev();
+                    });
+                    handleFirstSlide(swiper);
+                    swiper.on('slideChangeTransitionStart', () => {
+                        handleSlideChange(swiper)
+                    });
+                    fs = false;
+                    event.currentTarget.removeEventListener("click", quitFsMode);
+                    event.currentTarget.addEventListener("click", enterFsMode);
+                    const photos = document.querySelectorAll(".photo");
+                    photos.forEach((photo) => {
+                        photo.style.height = "";
+                        photo.style.width = "";
+                    });
+                    const main = document.querySelector("main");
+                    main.style.overflowY = "";
+                    const logo = document.getElementById("pdb-logo-container");
+                    const burger = document.getElementById("pdb-burger");
+                    burger.style.zIndex = "98";
+                    logo.style.zIndex = "98";
+                    slider.classList.remove("full-screen");
+                    slider.classList.add("normal");
                 }
 
                 nextBtn.addEventListener("click", () => {
@@ -75,8 +172,10 @@ const section = document.getElementById("pdb-photos");
                     swiper.slidePrev();
                 });
 
-                handleFirstSlide();
-                swiper.on('slideChangeTransitionStart', handleSlideChange);
+                handleFirstSlide(swiper);
+                swiper.on('slideChangeTransitionStart', () => {
+                    handleSlideChange(swiper)
+                });
             }
             initSwiper();
         }) 
